ff7cee73a9431e4a69a4a3ec8016aca420829adb
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-02-20 11:02:19 -0800

    Implement delete route

--- app.js
+++ app.js
@@ -92,6 +92,17 @@ app.put('/blogs/:id', function(req, res){
     });
 });
 
+// DELETE
+app.delete('/blogs/:id', function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
+        }
+    });
+});
+
 app.listen(3000, function(){
     console.log('Server has started');
 });
